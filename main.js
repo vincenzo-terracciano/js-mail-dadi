c61abe0ea427f9e4bdff52942f5723d3d42df995
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-13 15:13:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Crea una lista di email di invitati ad una festa
+let = ["lost@gmail.com, vikings@libero.it, harrypotter@live.it, goku@gmail.com"];
+
+// Chiedi all’utente la sua email
+
+// controlla che sia nella lista di chi può accedere
+
+// stampa un messaggio appropriato sull’esito del controllo

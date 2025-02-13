4286fb7cc9f46ea6071d2af391cbdbdb042137a8
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-13 15:31:42 +0100

    add email exercise

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 // Crea una lista di email di invitati ad una festa
-let = ["lost@gmail.com, vikings@libero.it, harrypotter@live.it, goku@gmail.com"];
+let mailList = ["lost@gmail.com", "vikings@libero.it", "harrypotter@live.it", "goku@gmail.com"];
 
 // Chiedi all’utente la sua email
+const userEmail = prompt("Inserisci la tua email");
+console.log(userEmail);
+
+
+// Dichiarare una variabile con condizione TRUE
+let x;
 
 // controlla che sia nella lista di chi può accedere
+for (let i = 0; i < mailList.length; i++) {
+    const email = mailList[i];
+    if (email === userEmail) {
+        x = true
+    }
+}
 
 // stampa un messaggio appropriato sull’esito del controllo
+if (x == true) {
+    console.log(`Ciao ${userEmail}, l'accesso è consentito`);
+    
+} else {
+    console.log(`Mi dispiace ${userEmail}, l'accesso non è consentito`);
+    
+}
